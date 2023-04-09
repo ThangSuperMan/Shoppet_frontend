c99bcafac9b4f14f24b08d5025ec940c3b3c1469
thangsuperman <thangjenny2002@gmail.com>
2023-04-09 10:53:51 +0700

    feat: added bottom navigation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
 import { BannerComponent } from './banner/banner.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LogoComponent,
-    BannerComponent
+    BannerComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
