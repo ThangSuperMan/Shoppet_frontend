7f43dbac114dfe952a4650672711edf8067a86c2
thangsuperman <thangjenny2002@gmail.com>
2023-05-12 15:57:38 +0700

    feat: wrapped up the ui for add card (in payment page)

--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { GeolocationService } from 'src/app/_services/geolocation/geolocation.service';
 import { Location } from '@models';
 
@@ -9,7 +9,11 @@ import { Location } from '@models';
 })
 export class PaymentComponent {
   isShowAddAddressWindow: boolean = false;
+  isShowAddCardWindow: boolean = true;
+  expirationMonthActiveNumber: string = '01';
+  expirationYearActiveNumber: number = new Date().getFullYear();
   years: number[] | undefined;
+  months: string[] = [];
   lat = 10.82302;
   lng = 106.62965;
   latitudePicker = 10.82302;
@@ -20,18 +24,49 @@ export class PaymentComponent {
 
   constructor(private geolocationService: GeolocationService) {
     const currentYear = new Date().getFullYear();
+    const zero = '0';
     this.years = Array.from({ length: 10 }, (_, index) => currentYear + index);
+    for (let index = 1; index <= 12; index++) {
+      if (index <= 9) {
+        this.months?.push(zero + index);
+      } else {
+        this.months?.push(index.toString());
+      }
+    }
+    console.log('months :>> ', this.months);
   }
 
   ngOnInit() {}
 
+  handleClickExiprationMonth(month: string): void {
+    console.log('handleClickExiprationMonth');
+    console.log('month :>> ', month);
+    this.expirationMonthActiveNumber = month;
+  }
+
+  handleClickExiprationYear(year: number): void {
+    console.log('handleClickExiprationYear');
+    console.log('month :>> ', year);
+    this.expirationYearActiveNumber = year;
+  }
+
   handleClickOverlay(): void {
     console.log('handleClickOverlay');
     this.isShowAddAddressWindow = false;
+    this.isShowAddCardWindow = false;
+  }
+
+  handleAddCreditOrDebitCard(): void {
+    console.log('handleAddCreditOrDebitCard');
+    this.isShowAddCardWindow = true;
   }
 
   handleAddAddress(): void {
     this.isShowAddAddressWindow = true;
+  }
+
+  handleCloseCardWindow(): void {
+    this.isShowAddCardWindow = false;
   }
 
   handleCloseAddressWindow(): void {
