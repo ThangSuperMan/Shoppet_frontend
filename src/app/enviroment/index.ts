4c12e37fb52062409afd4f4ff5c7997d2b5fafe4
thangsuperman <thangjenny2002@gmail.com>
2023-06-03 17:25:01 +0700

    feat: add new server api (after deployed)

--- src/app/enviroment/index.ts
+++ src/app/enviroment/index.ts
@@ -1,5 +1,6 @@
 const enviroment = {
-  pathApi: 'http://localhost:3001/api/v1',
+  pathApi: 'http://54.163.139.142:3001/api/v1',
+  // pathApi: 'http://localhost:3001/api/v1',
   googleMapApiKey: 'AIzaSyAjFS3c_q0ub5-ziyV4jXzdknmpNPa4bDk',
 };
 
